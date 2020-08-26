88b1b057035ee31baf6e291a46a1ac1fee2eadcc
EASTPROSZ\xiangyu.gao <xiangyu.gao@eabsystems.com>
2020-08-26 16:25:20 +0800

    update view.

--- app/controller/view/aura_controller.js
+++ app/controller/view/aura_controller.js
@@ -101,15 +101,25 @@ exports.api = {
           }
         });
         if (JSON.parse(keys)[1] === 'PH') {
+          const newRows3 = [];
           _.forEach(newRows, (obj) => {
             obj.key[1] = 'PH';
             obj.key[2] = obj.value.pIdCardNo;
+            if (JSON.parse(keys)[2] === obj.key[2]) {
+              newRows3.push(obj);
+            }
           });
-          resultTemp.total_rows = newRows.length;
-          resultTemp.rows = newRows;
+          resultTemp.total_rows = newRows3.length;
+          resultTemp.rows = newRows3;
         } else if (JSON.parse(keys)[1] === 'LA') {
-          resultTemp.total_rows = newRows2.length;
-          resultTemp.rows = newRows2;
+          const newRows3 = [];
+          _.forEach(newRows2, (obj) => {
+            if (JSON.parse(keys)[2] === obj.key[2]) {
+              newRows3.push(obj);
+            }
+          });
+          resultTemp.total_rows = newRows3.length;
+          resultTemp.rows = newRows3;
         }
         res.json(resultTemp);
       }
@@ -191,29 +201,40 @@ exports.api = {
           } else {
             iIdCardNo = obj.value.pIdCardNo;
           }
-          if (obj.value.pIdCardNo !== null) {
-            obj.key = ['01', 'PH', obj.value.pIdCardNo];
+          if (obj.value.pName !== null && obj.value.pDob !== null) {
+            obj.key = ['01', 'PH', obj.value.pName, obj.value.pDob];
             obj.value.iIdCardNo = iIdCardNo;
             delete obj.value.insured;
             newRows.push(obj);
           }
-          if (iIdCardNo !== null || obj.value.sameAs === 'Y') {
-            obj.key = ['01', 'LA', iIdCardNo];
+          if ((obj.value.iName !== null && obj.value.iDob !== null) || obj.value.sameAs === 'Y') {
+            obj.key = ['01', 'LA', obj.value.iName, obj.value.iDob];
             obj.value.iIdCardNo = iIdCardNo;
             delete obj.value.insured;
             newRows2.push(obj);
           }
         });
         if (JSON.parse(keys)[1] === 'PH') {
+          const newRows3 = [];
           _.forEach(newRows, (obj) => {
             obj.key[1] = 'PH';
-            obj.key[2] = obj.value.pIdCardNo;
+            obj.key[2] = obj.value.pName;
+            obj.key[3] = obj.value.pDob;
+            if (JSON.parse(keys)[2] === obj.key[2] && JSON.parse(keys)[3] === obj.key[3]) {
+              newRows3.push(obj);
+            }
           });
-          resultTemp.total_rows = newRows.length;
-          resultTemp.rows = newRows;
+          resultTemp.total_rows = newRows3.length;
+          resultTemp.rows = newRows3;
         } else if (JSON.parse(keys)[1] === 'LA') {
-          resultTemp.total_rows = newRows2.length;
-          resultTemp.rows = newRows2;
+          const newRows3 = [];
+          _.forEach(newRows2, (obj) => {
+            if (JSON.parse(keys)[2] === obj.key[2] && JSON.parse(keys)[3] === obj.key[3]) {
+              newRows3.push(obj);
+            }
+          });
+          resultTemp.total_rows = newRows3.length;
+          resultTemp.rows = newRows3;
         }
         res.json(resultTemp);
       }
